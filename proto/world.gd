056ad03c3600785ce2f95ad4c8cd35f07174977e
John Smolinski <jsmo81@iastate.edu>
2019-10-16 13:10:38 -0500

    abstracting out _ready method in node 2d

--- proto/world.gd
+++ proto/world.gd
@@ -1,57 +1,62 @@
 extends Node2D
 
-# Declare member variables here.
+#Loading in sprites
 var major_city_good = preload("res://major_city_good.png");
 var major_city_bad = preload("res://major_city_bad.png");
+
+#Loading objects to be set to proper nodes
+onready var mjr_btm_left = get_node("mjr_btm_left");
+onready var mjr_middle = get_node("mjr_middle");
+onready var mjr_top_right = get_node("mjr_top_right");
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	generate_city_alliance();
+
+func generate_city_alliance():
+	set_up_alliances();
+	check_for_same();
+
+func set_up_alliances():
 	randomize();
-	var mjr_btm_left = get_node("mjr_btm_left");
-	var mjr_middle = get_node("mjr_middle");
-	var mjr_top_right = get_node("mjr_top_right");
-	
-	if(randi()%101 + 1 < 50):
+	if(randi()%100 + 1 < 50):
 		mjr_btm_left.texture = major_city_good;
 	else:
 		mjr_btm_left.texture = major_city_bad;
 		
-	if(randi()%101 + 1 < 50):
+	if(randi()%100 + 1 < 50):
 		mjr_middle.texture = major_city_good;
 	else:
 		mjr_middle.texture = major_city_bad;
 		
-	if(randi()%101 + 1 < 50):
+	if(randi()%100 + 1 < 50):
 		mjr_top_right.texture = major_city_good;
 	else:
 		mjr_top_right.texture = major_city_bad;
-		
+
+func check_for_same():
+	randomize();
 	if(mjr_top_right.texture == major_city_bad
 		&& mjr_middle.texture == major_city_bad
 		&& mjr_btm_left.texture == major_city_bad):
-			var choice = randi() % 4 + 1;
+			var choice = randi() % 3 + 1;
 			if(choice  == 1):
 				mjr_top_right.texture = major_city_good;
 			if(choice == 2):
 				mjr_middle.texture = major_city_good;
 			if(choice == 3):
 				mjr_btm_left.texture = major_city_good;
-	
 	if(mjr_top_right.texture == major_city_good
 		&& mjr_middle.texture == major_city_good
 		&& mjr_btm_left.texture == major_city_good):
-			var choice = randi() % 4 + 1;
+			var choice = randi() % 3 + 1;
 			if(choice  == 1):
 				mjr_top_right.texture = major_city_bad;
 			if(choice == 2):
 				mjr_middle.texture = major_city_bad;
 			if(choice == 3):
 				mjr_btm_left.texture = major_city_bad;
-
-
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
